11cc12c02b4497366fabb584866140993738bd45
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2022-05-27 13:13:31 +0100

    hw/cxl: Make the CXL fixed memory window setup a machine parameter.
    Paolo Bonzini requested this change to simplify the ongoing
    effort to allow machine setup entirely via RPC.
    
    Includes shortening the command line form cxl-fixed-memory-window
    to cxl-fmw as the command lines are extremely long even with this
    change.
    
    The json change is needed to ensure that there is
    a CXLFixedMemoryWindowOptionsList even though the actual
    element in the json is never used. Similar to existing
    SgxEpcProperties.
    
    Update cxl-test and bios-tables-test to reflect new parameters.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- hw/cxl/cxl-host-stubs.c
+++ hw/cxl/cxl-host-stubs.c
@@ -7,10 +7,6 @@
 #include "qapi/error.h"
 #include "hw/cxl/cxl.h"
 
-void cxl_fixed_memory_window_config(MachineState *ms,
-                                    CXLFixedMemoryWindowOptions *object,
-                                    Error **errp) {};
-
 void cxl_fixed_memory_window_link_targets(Error **errp) {};
 
 const MemoryRegionOps cfmws_ops;
