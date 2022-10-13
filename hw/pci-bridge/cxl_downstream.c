b70340fcef3b1e2c47ddd059e89ab08aca5bc90c
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2022-10-13 15:47:53 +0100

    hw/cxl/mailbox: Implement Physical Switch Identify
    For now our switch is fixed function making this implement
    simple.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- hw/pci-bridge/cxl_downstream.c
+++ hw/pci-bridge/cxl_downstream.c
@@ -14,6 +14,7 @@
 #include "hw/pci/pcie.h"
 #include "hw/pci/pcie_port.h"
 #include "qapi/error.h"
+#include "hw/cxl/cxl.h"
 
 typedef struct CXLDownstreamPort {
     /*< private >*/
@@ -22,9 +23,6 @@ typedef struct CXLDownstreamPort {
     /*< public >*/
     CXLComponentState cxl_cstate;
 } CXLDownstreamPort;
-
-#define TYPE_CXL_DSP "cxl-downstream"
-DECLARE_INSTANCE_CHECKER(CXLDownstreamPort, CXL_DSP, TYPE_CXL_DSP)
 
 #define CXL_DOWNSTREAM_PORT_MSI_OFFSET 0x70
 #define CXL_DOWNSTREAM_PORT_MSI_NR_VECTOR 1
