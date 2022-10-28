c0cbf2ffd73a5c72bfdfa7604a6a3809fee5c0b9
Ira Weiny <ira.weiny@intel.com>
2022-10-28 13:01:41 -0700

    hw/cxl/events: Add Mock events.

--- include/hw/cxl/cxl_device.h
+++ include/hw/cxl/cxl_device.h
@@ -413,4 +413,6 @@ void cxl_event_insert(struct cxl_event_log *log,
 void cxl_event_delete_head(struct cxl_event_log *log);
 void cxl_event_irq_assert(CXLType3Dev *ct3d);
 
+void cxl_mock_add_event_logs(CXLDeviceState *cxlds);
+
 #endif
