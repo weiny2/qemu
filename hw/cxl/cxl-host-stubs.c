4c156d3f2c13aee9df41240d6c60d864544507c9
Ira Weiny <ira.weiny@intel.com>
2022-10-31 16:25:18 -0700

    WIP hw/cxl/events: Add hmp command to assert interrupts
    Reset all logs and generate an interrupt.
    
    This is a WIP as injecting events needs to be a QMP command eventually.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- hw/cxl/cxl-host-stubs.c
+++ hw/cxl/cxl-host-stubs.c
@@ -12,4 +12,9 @@ void cxl_fmws_link_targets(CXLState *stat, Error **errp) {};
 void cxl_machine_init(Object *obj, CXLState *state) {};
 void cxl_hook_up_pxb_registers(PCIBus *bus, CXLState *state, Error **errp) {};
 
+void hmp_cxl_event_inject(Monitor *mon, const QDict *qdict)
+{
+    monitor_printf(mon, "CXL devices not supported\n");
+}
+
 const MemoryRegionOps cfmws_ops;
