78523495769f9e94e91f0bf4bbdfe48fb5d26034
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2022-11-17 13:26:06 +0000

    hw/cxl: Initial poison injection support.
    Inject poison using qmp command cxl-inject-poison to
    add an entry to the poison list.
    
    For now, the poison is not returned CXL.mem reads, but only via
    the mailbox command Get Poison List.
    
    See CXL rev 3.0, sec 8.2.9.8.4.1 Get Poison list (Opcode 4300h)
    
    Kernel patches to use this interface here:
    https://lore.kernel.org/linux-cxl/cover.1665606782.git.alison.schofield@intel.com/
    
    RFC for now as likely the implementation will change as
    support for mailbox based poison injection and media scanning are added.
    
    To inject poison using qmp (telnet to the qmp port)
    { "execute": "qmp_capabilities" }
    
    { "execute": "cxl-inject-poison",
        "arguments": {
             "path": "/machine/peripheral/cxl-pmem0",
             "start": 2048,
             "length": 256
        }
    }
    
    Adjusted to select a device on your machine.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@huawei.com>

---
+++ hw/mem/cxl_type3_stubs.c
@@ -0,0 +1,6 @@
+
+#include "qemu/osdep.h"
+#include "qapi/qapi-commands-cxl.h"
+
+void qmp_cxl_inject_poison(const char *path, uint64_t start, uint64_t length,
+                           Error **errp) {}
